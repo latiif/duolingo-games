ceb3a5d793172d9eac62eccdad32d6da046ef841
Latiif <llusx@outlook.com>
2019-02-18 11:46:56 +0100

    Add login bean

--- duogames/src/main/java/cth/webapp/duogames/duogames/control/LoginBean.java
+++ duogames/src/main/java/cth/webapp/duogames/duogames/control/LoginBean.java
@@ -7,15 +7,52 @@ package cth.webapp.duogames.duogames.control;
 
 import com.latiif.duoapi.DuoApi;
 import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
 
 
 /**
  *
  * @author latiif
  */
+
+@Named("login")
+//@RequestScoped
+@SessionScoped
 public class LoginBean implements Serializable{
     
+    private DuoApi user;
+    
+    @Getter
+    @Setter
+    private String username;
+    
+    @Getter
+    @Setter
+    private String password;
+    
+
+    @Getter
+    private boolean isLoggedIn = false;
+    
     public LoginBean(){
+        username = "";
+        password = "";
+    }
+    
+    
+    /*
+    
+    */
+    public void login(){
+        if (username.isEmpty() || password.isEmpty())
+            return;
+        
+        user = new DuoApi(username, password);
+        
+        isLoggedIn = user.getIsLoggedIn();
     }
     
 }
