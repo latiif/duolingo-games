0a6cdcd78eef0a57c8befb8210126107d8ef947a
Latiif <llusx@outlook.com>
2019-02-18 11:39:47 +0100

    Add duoapi jar dependency
    Add local dependency to maven

---
+++ duogames/src/main/java/cth/webapp/duogames/duogames/control/LoginBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cth.webapp.duogames.duogames.control;
+
+import com.latiif.duoapi.DuoApi;
+import java.io.Serializable;
+
+
+/**
+ *
+ * @author latiif
+ */
+public class LoginBean implements Serializable{
+    
+    public LoginBean(){
+    }
+    
+}
