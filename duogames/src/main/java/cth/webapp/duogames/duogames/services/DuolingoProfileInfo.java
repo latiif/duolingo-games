c1c1fad7f60e9bf3eea0eceef5431fdd3f477e4c
Latiif <llusx@outlook.com>
2019-02-22 15:28:56 +0100

    Add component for profile card

--- duogames/src/main/java/cth/webapp/duogames/duogames/services/DuolingoProfileInfo.java
+++ duogames/src/main/java/cth/webapp/duogames/duogames/services/DuolingoProfileInfo.java
@@ -7,6 +7,8 @@ package cth.webapp.duogames.duogames.services;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 
 /**
@@ -14,6 +16,7 @@ import lombok.Getter;
  * @author latiif
  */
 public class DuolingoProfileInfo {
+
     @Getter
     private String username;
     @Getter
@@ -26,38 +29,64 @@ public class DuolingoProfileInfo {
     private Integer rupees;
     @Getter
     private String created;
-    
+
     @Getter
     private String imgurl;
 
+    @Getter
+    private Integer streak;
+
+    @Getter
+    private List<String> languages;
+
     public DuolingoProfileInfo(JsonObject userData) {
-        if (userData==null)
+        if (userData == null) {
             return;
-        
-        this.username =  safeRetrieve(userData, "username", "duolearner");
+        }
+
+        this.username = safeRetrieve(userData, "username", "duolearner");
         this.fullname = safeRetrieve(userData, "fullname", "Language Learner");
         this.bio = safeRetrieve(userData, "bio", " ");
         this.location = safeRetrieve(userData, "location", "Earth");
-        
+
         this.rupees = Integer.valueOf(safeRetrieve(userData, "rupees", "0"));
-        
+
+        this.streak = Integer.valueOf(safeRetrieve(userData, "site_streak", "0"));
+
         this.created = safeRetrieve(userData, "created", "Sometime in the past");
-        this.created = this.created.replace("\n","");
-        
+        this.created = this.created.replace("\n", "");
+
         String url = safeRetrieve(userData, "avatar", "//s3.amazonaws.com/duoli…images/avatar/default_2");
-        this.imgurl  =  "https:" + url + "/xlarge";
+        this.imgurl = "https:" + url + "/xlarge";
         
+        this.languages = getLanguages(true, userData);
+
     }
-    
-    
-    private String safeRetrieve(JsonObject userData, String field, String defaulValue){
+
+    private List<String> getLanguages(boolean inAbbr, JsonObject userData) {
+        List<String> res = new ArrayList<String>();
+        if (userData != null) {
+            for (JsonElement element : userData.getAsJsonArray("languages")) {
+                if (element.getAsJsonObject().get("learning").getAsBoolean()) {
+                    if (inAbbr) {
+                        res.add(element.getAsJsonObject().get("language").getAsString());
+                    } else {
+                        res.add(element.getAsJsonObject().get("language_string").getAsString());
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    private String safeRetrieve(JsonObject userData, String field, String defaulValue) {
         JsonElement element = userData.get(field);
-        
-        if (element.isJsonNull()){
+
+        if (element.isJsonNull()) {
             return defaulValue;
         }
-        
+
         return element.getAsString();
     }
-    
+
 }
