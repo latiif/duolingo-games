568344b384e83c440490a4677eb9ce9a01b6d627
Stina Markan <stinamarkan@gmail.com>
2019-03-01 08:50:21 +0100

    Quizzes can now be finished, and you can play again

--- duogames/src/main/java/cth/webapp/duogames/duogames/control/QuizBean.java
+++ duogames/src/main/java/cth/webapp/duogames/duogames/control/QuizBean.java
@@ -7,10 +7,14 @@ package cth.webapp.duogames.duogames.control;
 
 import cth.webapp.duogames.duogames.model.quiz.Question;
 import cth.webapp.duogames.duogames.model.quiz.Quiz;
+import java.io.IOException;
 import java.io.Serializable;
+import java.math.BigInteger;
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import lombok.Getter;
@@ -24,8 +28,9 @@ import net.bootsfaces.utils.FacesMessages;
 @Named(value="quiz")
 @SessionScoped
 public class QuizBean implements Serializable {
-
-   
+    
+    @Inject
+    private UserBean userBean;
     
     private List<Question> quiz;
     
@@ -41,48 +46,66 @@ public class QuizBean implements Serializable {
     @Setter
     private int currQuestion;
     
-    
     @Getter
     private int nrCorrect;
     
-    @Inject
-    private UserBean userBean;
+    @Getter
+    private String time;
+    
+    private Timestamp startTime;
+    private Timestamp endTime;
+    
 
     public List<Question> getQuizInformation(UserBean ub) {
-        
-        
         
         if (quiz == null) {
             quiz = startQuiz();
         }
-
         
         return quiz;
     }
 
     public List<Question> startQuiz() {
        
-
         Map<String, List<String>> dict = userBean.getApi().getDictionaryOfKnownWords("en", userBean.getApi().getCurrentLanguage());
 
         currQuestion = 0;
         nrCorrect = 0;
         totalQuestions = 10;
+        startTime = new Timestamp(System.currentTimeMillis());
         
         return new Quiz(dict, 10, 3).generateQuestions();
 
     }
     
+    public void resetQuiz() {
+        quiz = null;
+        redirect("/duogames/play.xhtml");
+    }
+    
     public void validate(){
         if (quiz.get(currQuestion++).check(answer))
         {
+            if(currQuestion == 10){
+                endTime = new Timestamp(System.currentTimeMillis());
+                // = BigInteger.valueOf((endTime.getTime() - startTime.getTime()) / 1000);
+                long diff = (endTime.getTime() - startTime.getTime()) / 1000L;
+                time = String.format("%02d:%02d:%02d", diff / 3600, (diff % 3600) / 60, diff % 60);
+                redirect("/duogames/score.xhtml");
+            }
             FacesMessages.info("Correct!");
             nrCorrect++;
         }
         else{
             FacesMessages.error("Wrong");
         }
-        
-        
     }
+
+    private void redirect(String url) {
+        try {
+                    FacesContext.getCurrentInstance().getExternalContext().redirect(url);
+                } catch (IOException e) {
+                    System.err.println(e.getMessage());
+                }
+        }
 }
