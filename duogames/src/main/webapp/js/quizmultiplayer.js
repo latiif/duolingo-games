ce84df5cc588e4344e1f16838ff09d109003eb9b
Latiif <llusx@outlook.com>
2019-03-11 19:27:23 +0100

    Add progressbar and realtime result showing

--- duogames/src/main/webapp/js/quizmultiplayer.js
+++ duogames/src/main/webapp/js/quizmultiplayer.js
@@ -7,20 +7,37 @@
 
 var websocketSession;
 
+var standings;
+
+var currQuestion;
+
 function f_onmessage(evt) {
-    websocketMessages = document.getElementById('eventlog');
+    websocketMessages = document.getElementById('standings');
     
     var info = JSON.parse(evt.data);
     
-    var msg = `User ${info.username} is now at question nr ${info.currQuestion} out of which ${info.numCorrect} is/are correct`;
+    var html = ''
     
-    websocketMessages.innerHTML = websocketMessages.innerHTML + msg + '<br/>';
+    console.log(info)
+    
+    standings[`${info.username}`] = info;
+    
+    for (var user in standings){
+        if (standings.hasOwnProperty(`${user}`)){
+            userinfo = standings[`${user}`];
+            html += `<p>${user}</p><div class="progress"><div class="progress-bar progress-bar-success" role="progressbar" style="width:${userinfo.numCorrect*10}%"></div><div class="progress-bar progress-bar-warning" role="progressbar" style="width:${(userinfo.currQuestion-userinfo.numCorrect) * 10}%"></div></div></br>`
+        }
+    }
+    
+    websocketMessages.innerHTML = html;
 }
 
 function open(gameid) {
     if (!websocketSession) {
         websocketSession = new WebSocket('ws://' + document.location.host  +`/duogames/quizmultiplayer/${gameid}`);
         websocketSession.onmessage = f_onmessage;
+        standings = {}
+        currQuestion = 1;
     }
 }
 
@@ -30,10 +47,10 @@ function close() {
     }
 }
 
-function sendMessage(userName,currentQuestion,numberCorrect) {
+function sendMessage(userName,numberCorrect) {
     msg = {
         username: userName,
-        currQuestion: currentQuestion,
+        currQuestion: currQuestion++,
         numCorrect: numberCorrect
     };
     
