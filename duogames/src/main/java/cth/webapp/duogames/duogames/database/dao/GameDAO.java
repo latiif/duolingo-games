51ccad8539fec18179525ef44f7a55f7169470d6
nicket97 <niclas.o.johansson@telia.com>
2019-03-15 15:36:29 +0100

    test for check

--- duogames/src/main/java/cth/webapp/duogames/duogames/database/dao/GameDAO.java
+++ duogames/src/main/java/cth/webapp/duogames/duogames/database/dao/GameDAO.java
@@ -11,44 +11,43 @@ import javax.persistence.Query;
 
 @Stateless
 public class GameDAO extends AbstractDAO<GameSession> {
-    
+
     @Inject
     UserBean user;
-   
+
     public GameDAO() {
         super(GameSession.class);
     }
-    
-    public List<GameSession> findHighestScores(int range, String type){
+
+    public List<GameSession> findHighestScores(int range, String type) {
         Query q = em.createQuery("SELECT g FROM GameSession g WHERE g.type = :type ORDER BY g.score DESC");
-        q.setParameter("type",type);
+        q.setParameter("type", type);
         q.setMaxResults(range);
         return q.getResultList();
     }
-    
-    public int findSingleHighestScore(){
+
+    public int findSingleHighestScore() {
         Query q = em.createQuery("SELECT MAX(g.score) FROM GameSession g WHERE g.userid = :userid");
-        q.setParameter("userid",user.getUser());
+        q.setParameter("userid", user.getUser());
         Integer i = (int) q.getSingleResult();
         return i;
     }
-    
-    public BigInteger findQuickestTime(){
+
+    public BigInteger findQuickestTime() {
         Query q = em.createQuery("SELECT MIN(g.time) FROM GameSession g WHERE g.userid = :userid");
-        q.setParameter("userid",user.getUser());
+        q.setParameter("userid", user.getUser());
         return Optional.ofNullable((BigInteger) q.getSingleResult()).orElse(BigInteger.valueOf(0));
     }
-    
-    public BigInteger findSlowestTime(){
+
+    public BigInteger findSlowestTime() {
         Query q = em.createQuery("SELECT MAX(g.time) FROM GameSession g WHERE g.userid = :userid");
-        q.setParameter("userid",user.getUser());
+        q.setParameter("userid", user.getUser());
         return Optional.ofNullable((BigInteger) q.getSingleResult()).orElse(BigInteger.valueOf(0));
     }
-    
 
     public int findTotalGames() {
         Query q = em.createQuery("SELECT COUNT(g) FROM GameSession g WHERE g.userid = :userid");
-        q.setParameter("userid",user.getUser());
+        q.setParameter("userid", user.getUser());
         long l = (long) q.getSingleResult();
         return (int) l;
     }
--- duogames/src/test/java/cth/webapp/duogames/duogames/test/QuizTest.java
+++ duogames/src/test/java/cth/webapp/duogames/duogames/test/QuizTest.java
@@ -8,12 +8,16 @@ package cth.webapp.duogames.duogames.test;
 import cth.webapp.duogames.duogames.control.QuizBean;
 import cth.webapp.duogames.duogames.control.UserBean;
 import cth.webapp.duogames.duogames.model.IQuestion;
+import cth.webapp.duogames.duogames.model.quiz.Question;
 import cth.webapp.duogames.duogames.services.DuoApi;
 import cth.webapp.duogames.duogames.view.QuizData;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Init;
 import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,31 +29,46 @@ import org.junit.Test;
  */
 public class QuizTest {
     private QuizBean quiz = new QuizBean();
-     List<IQuestion> quizList;
-     UserBean user;
-     QuizData quizData;
      DuoApi api = new DuoApi("llusx", "NO_PASSWORD_NEEDED");
      
     @Init
     public void initTest(){
-        user = new UserBean();
-        user.signin();
-        user.setApi(api);
-        quizData = new QuizData();
-        quiz.setQuizData(quizData);
-        quizList = quiz.startGame();
+
     }
     @Test 
     public void getDictionary(){
-        user = new UserBean();
-        //user.signin();
-        user.setApi(api);
-        quizData = new QuizData();
-        quiz.setQuizData(quizData);
-        quizList = quiz.startGame();
-        quiz.validate();
+        int x = 0;
+        List<String> wrong = new ArrayList<>();
+        wrong.add("a");
+        wrong.add("b");
+        wrong.add("c");
         
-        System.out.println(api.getDictionaryOfKnownWords("en", api.getCurrentLanguage()));
+        
+
+        Question quizl = new Question("test",wrong, "test");
+        assertTrue(quizl.check("test"));
+        assertFalse(quizl.check("a"));
+        assertFalse(quizl.check("b"));
+        assertFalse(quizl.check("c"));
+        assertFalse(quizl.check("d"));
+        
+        
+        quizl = new Question("test",wrong, "töst");
+        assertTrue(quizl.check("töst"));
+        assertFalse(quizl.check("test"));
+        assertFalse(quizl.check("b"));
+        assertFalse(quizl.check("c"));
+        assertFalse(quizl.check("d"));
+        
+        quizl = new Question("test",wrong, "b");
+        assertTrue(quizl.check("b"));
+        assertFalse(quizl.check("test"));
+        assertFalse(quizl.check("a"));
+        assertFalse(quizl.check("c"));
+        assertFalse(quizl.check("d"));
+        
+        
+        //System.out.println(api.getDictionaryOfKnownWords("en", api.getCurrentLanguage()));
         
     }
 
